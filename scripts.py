c68012ce318ef1333df551cdaecda61bfe78a688
Spencer Wyman <spencer.wyman7@gmail.com>
2020-08-29 11:12:50 -0700

    script for building training set from source file

--- scripts.py
+++ scripts.py
@@ -1,14 +1,17 @@
 from sys import argv
+from os import path
+from pathlib import Path
 from PIL import Image
 from wallpapermakers import wallpapermaker
+from wallpapermakers import group_names
 
 command = argv[1]
-group_name = argv[2]
-image_path = argv[3]
-
-im = Image.open(image_path)
 
 if command == 'show':
+
+    group_name = argv[2]
+    image_path = argv[3]
+    im = Image.open(image_path)
     wallpapermaker(group_name, im).show()
 
 elif command == 'saveall':
@@ -28,3 +31,22 @@ elif command == 'saveall':
     ]
     for [tilename, image] in tilings:
         image.save('images/tilename')
+
+elif command == 'make_training_set':
+    source_path = Path(argv[2])
+    images = [Image.open(im_path) for im_path in source_path.iterdir()]
+
+    # create a directory for each tiling
+    for group in group_names:
+        tiling_path = source_path.parent / group
+        if not tiling_path.is_dir():
+            tiling_path.mkdir()
+
+    # create the tilings for each image and save to the created directories
+    for im in images:
+
+        for group in group_names:
+            im_name = path.basename(im.filename)
+            tiling_path = source_path.parent / group / (group + im_name)
+            tiling = wallpapermaker(group, im)
+            tiling.save(tiling_path)
